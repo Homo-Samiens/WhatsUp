ad241d9dffe4ab9dbb2cd41588aa1d2b0a309bf9
Sameer <sameer.ali097643@gmail.com>
2024-03-19 15:08:11 +0530

    Project Started

---
+++ app/src/main/java/com/sam/whatsup/data/Events.kt
@@ -0,0 +1,4 @@
+package com.sam.whatsup.data
+
+class Events {
+}
---
+++ app/src/main/java/com/sam/whatsup/di/WUApplication.kt
@@ -0,0 +1,7 @@
+package com.sam.whatsup.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WUApplication: Application()
