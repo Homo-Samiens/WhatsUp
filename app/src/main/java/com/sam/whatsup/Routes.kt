bedac29484813f80e72f67520d695817bb18f334
Sameer <sameer.ali097643@gmail.com>
2024-03-20 13:28:50 +0530

    Screens, Routes Created

---
+++ app/src/main/java/com/sam/whatsup/Routes.kt
@@ -0,0 +1,17 @@
+package com.sam.whatsup
+
+sealed class DestinationScreen(var route: String) {
+    object SignUp: DestinationScreen("signUp")
+    object LogIn: DestinationScreen("logIn")
+    object Profile: DestinationScreen("profile")
+
+    object ChatList: DestinationScreen("chatList")
+    object SingleChat: DestinationScreen("singleChat/{chatId}"){
+        fun createRoute(chatId: String) = "singleChat/$chatId" //Fun to get Real-Chat-Id & use in the template.
+    }
+
+    object StatusList: DestinationScreen("statusList")
+    object SingleStatus: DestinationScreen("singleStatus/{statusId}"){
+        fun createRoute(statusId: String) = "singleStatus/$statusId"
+    }
+}
